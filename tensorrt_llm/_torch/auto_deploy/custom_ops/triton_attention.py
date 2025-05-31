5f4f79fc020b98b8208d3fa5f4e91a031651b780
Suguna Velury <178320438+sugunav14@users.noreply.github.com>
2025-05-31 01:15:31 +0000

    Formatting fix
    Signed-off-by: Suguna Velury <178320438+sugunav14@users.noreply.github.com>

--- tensorrt_llm/_torch/auto_deploy/custom_ops/triton_attention.py
+++ tensorrt_llm/_torch/auto_deploy/custom_ops/triton_attention.py
@@ -383,11 +383,8 @@ class TritonWithFlattenedInputs(AttentionDescriptor):
 
         # do a sanity check on the scale if it is not None, we only support the default scale
         # of 1/sqrt(head_dim) and so we should do an approximate check for that one
-        if not isinstance(scale, float):     
-            ad_logger.warning(
-                f"Provided scale is not a float and is not supported for TritonWithFlattenedInputs, "
-                f"Using default scale instead."
-            )
+        if not isinstance(scale, float):
+            ad_logger.warning("Provided scale is not a float, Using default scale instead.")
             scale = None
 
         return [
