751e53aff764eeba84ee0e049837d433a6d693ef
Lucas Liebenwein <11156568+lucaslie@users.noreply.github.com>
2025-09-26 16:53:58 -0700

    added anticapted path for new models on llm_models trt-llm CI
    Signed-off-by: Lucas Liebenwein <11156568+lucaslie@users.noreply.github.com>

--- tests/unittest/_torch/auto_deploy/_utils_test/_model_test_utils.py
+++ tests/unittest/_torch/auto_deploy/_utils_test/_model_test_utils.py
@@ -447,7 +447,7 @@ _SMALL_MODEL_CONFIGS = {
         },
     },
     "ibm-ai-platform/Bamba-9B-v2": {
-        "llm_models_subdir": "NO_SUBDIR",  # no synced to llm_models_root at the moment
+        "llm_models_subdir": "Bamba-9B-v2",
         "model_kwargs": {
             "torch_dtype": "bfloat16",
             "hidden_size": 64,
@@ -466,7 +466,7 @@ _SMALL_MODEL_CONFIGS = {
         },
     },
     "nvidia/NVIDIA-Nemotron-Nano-12B-v2": {
-        "llm_models_subdir": "NO_SUBDIR",  # no synced to llm_models_root at the moment
+        "llm_models_subdir": "NVIDIA-Nemotron-Nano-12B-v2",
         "model_kwargs": {
             "torch_dtype": "bfloat16",
             "hidden_size": 32,
