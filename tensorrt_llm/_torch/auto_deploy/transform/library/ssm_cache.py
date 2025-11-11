97c4d7c59fef606c261b5b136523fcf0aeed4583
Lucas Liebenwein <11156568+lucaslie@users.noreply.github.com>
2025-11-10 16:42:23 -0800

    prototype for chunk_size
    Signed-off-by: Lucas Liebenwein <11156568+lucaslie@users.noreply.github.com>

--- tensorrt_llm/_torch/auto_deploy/transform/library/ssm_cache.py
+++ tensorrt_llm/_torch/auto_deploy/transform/library/ssm_cache.py
@@ -1,5 +1,11 @@
 """A set of transforms to handle SSM cache transforms."""
 
+from typing import List
+
+from torch.fx import GraphModule, Node
+
+from ...custom_ops.attention_interface import Constant
+from ...utils.node_utils import extract_op_args, is_op
 from ..interface import TransformRegistry
 from .kvcache import InsertCachedAttention
 
@@ -8,6 +14,17 @@ from .kvcache import InsertCachedAttention
 @TransformRegistry.register("insert_cached_ssm_attention")
 class SSMCacheTransform(InsertCachedAttention):
     """A transform to handle SSM cache operations."""
+
+    def _process_get_metadata(
+        self, gm: GraphModule, m_args: List[str], const_args: List[Constant]
+    ) -> List[Node]:
+        mamba_nodes = [
+            n for n in gm.graph.nodes if is_op(n, self.attn_descriptor.get_source_attention_op())
+        ]
+
+        chunk_size = extract_op_args(mamba_nodes[0], "chunk_size")
+        const_args.append(chunk_size)
+        return super()._process_get_metadata(gm, m_args, const_args)
 
 
 @TransformRegistry.register("insert_cached_causal_conv")
