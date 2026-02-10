c95df617ee8658b5717081d517f4737a873f5a28
Balamurugan Marimuthu <246387390+bmarimuthu-nv@users.noreply.github.com>
2026-02-10 13:01:33 -0800

    add vision model
    Signed-off-by: Balamurugan Marimuthu <246387390+bmarimuthu-nv@users.noreply.github.com>

--- tensorrt_llm/_torch/auto_deploy/models/custom/__init__.py
+++ tensorrt_llm/_torch/auto_deploy/models/custom/__init__.py
@@ -1,7 +1,7 @@
 from .modeling_eagle import Eagle3DrafterForCausalLM
 from .modeling_nemotron_flash import NemotronFlashForCausalLM, NemotronFlashPreTrainedTokenizerFast
 from .modeling_nemotron_h import NemotronHForCausalLM
-from .modeling_qwen3_5_moe import Qwen3_5MoeForCausalLM
+from .modeling_qwen3_5_moe import Qwen3_5MoeForCausalLM, Qwen3_5MoeForConditionalGeneration
 
 __all__ = (
     "Eagle3DrafterForCausalLM",
@@ -9,4 +9,5 @@ __all__ = (
     "NemotronFlashPreTrainedTokenizerFast",
     "NemotronHForCausalLM",
     "Qwen3_5MoeForCausalLM",
+    "Qwen3_5MoeForConditionalGeneration",
 )
