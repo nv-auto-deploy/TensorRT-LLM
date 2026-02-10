b137f9f7871040c7436c6a0887d43a0e55d6e70c
Balamurugan Marimuthu <246387390+bmarimuthu-nv@users.noreply.github.com>
2026-02-10 11:28:08 -0800

    qwen3.5 clean modeling: text
    Signed-off-by: Balamurugan Marimuthu <246387390+bmarimuthu-nv@users.noreply.github.com>

--- tensorrt_llm/_torch/auto_deploy/models/custom/__init__.py
+++ tensorrt_llm/_torch/auto_deploy/models/custom/__init__.py
@@ -1,10 +1,12 @@
 from .modeling_eagle import Eagle3DrafterForCausalLM
 from .modeling_nemotron_flash import NemotronFlashForCausalLM, NemotronFlashPreTrainedTokenizerFast
 from .modeling_nemotron_h import NemotronHForCausalLM
+from .modeling_qwen3_5_moe import Qwen3_5MoeForCausalLM
 
 __all__ = (
     "Eagle3DrafterForCausalLM",
     "NemotronFlashForCausalLM",
     "NemotronFlashPreTrainedTokenizerFast",
     "NemotronHForCausalLM",
+    "Qwen3_5MoeForCausalLM",
 )
