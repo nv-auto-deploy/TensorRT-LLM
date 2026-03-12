230626b89632a734ad8ea69e7c9c59d007dcfd46
Balamurugan Marimuthu <246387390+bmarimuthu-nv@users.noreply.github.com>
2026-03-12 12:22:43 -0700

    ckpt
    Signed-off-by: Balamurugan Marimuthu <246387390+bmarimuthu-nv@users.noreply.github.com>

--- tensorrt_llm/_torch/auto_deploy/models/custom/__init__.py
+++ tensorrt_llm/_torch/auto_deploy/models/custom/__init__.py
@@ -1,4 +1,5 @@
 from .modeling_deepseek import DeepSeekV3ForCausalLM
+from .modeling_gemma3n import Gemma3nForCausalLM, Gemma3nForConditionalGeneration
 from .modeling_glm4_moe_lite import Glm4MoeLiteForCausalLM
 from .modeling_kimi_k2 import KimiK2ForCausalLM, KimiK25ForConditionalGeneration
 from .modeling_nemotron_flash import NemotronFlashForCausalLM, NemotronFlashPreTrainedTokenizerFast
@@ -7,6 +8,8 @@ from .modeling_qwen3_5_moe import Qwen3_5MoeForCausalLM, Qwen3_5MoeForConditiona
 
 __all__ = (
     "DeepSeekV3ForCausalLM",
+    "Gemma3nForCausalLM",
+    "Gemma3nForConditionalGeneration",
     "Glm4MoeLiteForCausalLM",
     "KimiK2ForCausalLM",
     "KimiK25ForConditionalGeneration",
